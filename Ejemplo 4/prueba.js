2538f9d8e6f444277bf26b9191606dd3340a286d
Felipe Esteban Alejandro Kessi Bustos <fkessi@udec.cl>
2023-07-19 15:54:38 -0400

    Ejemplo 3 y 4

---
+++ Ejemplo 4/prueba.js
@@ -0,0 +1,37 @@
+//Definición de Variable
+let x
+let y
+
+//Asignar datos a estas variables
+ x = "Hola Mundo "
+ y = "Hola Kervy"
+
+var z = x + y
+//Función Js que permite imprimir por consola 
+console.log(z)
+
+
+ x = 4
+ y = 5
+ 
+ z = x + y
+
+ console.log("El valor de la suma es : " + z)
+
+ //Tipos de Variables
+let String = "Esto es una cadena de texto"
+console.log(typeof(String))
+let Number = 26
+console.log(typeof(Number))
+let decimal = 0.2
+console.log(typeof(decimal))
+let booleanas = true
+console.log(typeof(booleanas))
+
+//Operaciones JS
+console.log(Number+decimal)
+console.log(Number*decimal)
+console.log(Number/decimal)
+console.log(Number-decimal)
+
+
